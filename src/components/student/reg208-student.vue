ba40b8c76dc8221a52ea66444a2748de499d373e
marnlikeagod016 <marnnew1999@gmail.com>
2021-04-13 16:10:21 +0700

    delete component comment and payment

--- src/components/student/reg208-student.vue
+++ src/components/student/reg208-student.vue
@@ -19,14 +19,6 @@
                 <RequestForm208 @onSubmit="onSubmit" />
               </div>
             </div>
-            <div class="col-6">
-              <div class="p-3 border bg-light h-100"><Comment208 /></div>
-            </div>
-            <div class="col-6">
-              <div class="p-3 border bg-light h-100">
-                <Payment />
-              </div>
-            </div>
           </div>
         </div>
       </div>
@@ -38,16 +30,12 @@
 import Navbar from "../student/navStudent.vue";
 import InformationForm from "./form/information.vue";
 import RequestForm208 from "./form/request-reg208.vue";
-import Comment208 from "./form/comment-reg208.vue";
-import Payment from "./form/payment.vue";
 import axios from "axios";
 export default {
   components: {
     Navbar,
     InformationForm,
     RequestForm208,
-    Comment208,
-    Payment,
   },
 
   data() {
@@ -56,16 +44,16 @@ export default {
       stphone: "",
       create_semester: "",
       create_academic_year: "",
-      checkbox_1:'',
-      checkbox_2:'',
-      from_semester:'',
-      from_academic:'',
-      to_semester:'',
-      to_academic:'',
-      radio_1:'',
-      radio_2:'',
-      re_text:'',
-      re_doc:''
+      checkbox_1: "",
+      checkbox_2: "",
+      from_semester: "",
+      from_academic: "",
+      to_semester: "",
+      to_academic: "",
+      radio_1: "",
+      radio_2: "",
+      re_text: "",
+      re_doc: "",
     };
   },
   methods: {
@@ -85,16 +73,16 @@ export default {
       // run function check เงื่อนไข gpax กับ radio2==true
       const path = "http://127.0.0.1:5000/send208";
       const formid = { formId: Date.now() };
-      const checkbox_1=this.checkbox_1
-      const checkbox_2=this.checkbox_2
-      const from_semester=this.from_semester
-      const from_academic=this.from_academic
-      const to_semester=this.to_semester
-      const to_academic=this.to_academic
-      const radio_1=this.radio_1
-      const radio_2=this.radio_2
-      const re_text=this.re_text
-      const re_doc=this.re_doc
+      const checkbox_1 = this.checkbox_1;
+      const checkbox_2 = this.checkbox_2;
+      const from_semester = this.from_semester;
+      const from_academic = this.from_academic;
+      const to_semester = this.to_semester;
+      const to_academic = this.to_academic;
+      const radio_1 = this.radio_1;
+      const radio_2 = this.radio_2;
+      const re_text = this.re_text;
+      const re_doc = this.re_doc;
       const {
         student_id,
         student_advisor_id,
@@ -104,7 +92,7 @@ export default {
       const phone = this.stphone;
       const create_semester = this.create_semester;
       const create_academic_year = this.create_academic_year;
-      const senddata = Object.assign({},formid, {
+      const senddata = Object.assign({}, formid, {
         phone,
         student_id,
         student_advisor_id,
@@ -121,15 +109,15 @@ export default {
         radio_1,
         radio_2,
         re_text,
-        re_doc
+        re_doc,
       });
-      console.log(senddata)
+      console.log(senddata);
       this.$confirm("Are you sure?").then(() => {
         axios
           .post(path, senddata)
           .then((res) => {
             console.log(res.data);
-            this.$alert("the request had sent")
+            this.$alert("the request had sent");
           })
           .catch((error) => {
             console.log(error);
@@ -142,16 +130,16 @@ export default {
       this.create_academic_year = value.create_academic_year;
     },
     onSubmit(value) {
-      this.checkbox_1=value.checkbox_1
-      this.checkbox_2=value.checkbox_2
-      this.from_semester=value.from_semester
-      this.from_academic=value.from_academic
-      this.to_semester=value.to_semester
-      this.to_academic=value.to_academic
-      this.radio_1=value.radio_1
-      this.radio_2=value.radio_2
-      this.re_text=value.re_text
-      this.re_doc=value.re_doc
+      this.checkbox_1 = value.checkbox_1;
+      this.checkbox_2 = value.checkbox_2;
+      this.from_semester = value.from_semester;
+      this.from_academic = value.from_academic;
+      this.to_semester = value.to_semester;
+      this.to_academic = value.to_academic;
+      this.radio_1 = value.radio_1;
+      this.radio_2 = value.radio_2;
+      this.re_text = value.re_text;
+      this.re_doc = value.re_doc;
       this.sendformInfo();
     },
   },
--- src/components/student/reg209-student.vue
+++ src/components/student/reg209-student.vue
@@ -1,106 +1,120 @@
 <template>
-	<div>
-		<NavStudent />
-		<div class="p-1 active-cont" style="text-align:center">
-			<div class="mx-4">
-				<div class="my-4">
-					<h4>REGISTRAR DIVISION, MAE FAH LUANG UNIVERSITY</h4>
-					<h4>Request Form for Re-entering Student</h4>
-				</div>
-				<!-- เดี๋ยวมาเพิ่ม -->
-				<div>
-					<p>Progress flow</p>
-				</div>
-				<!-- เดี๋ยวมาเพิ่ม -->
-				<div class="row gy-3 mx-5">
-					<div class="col-6">
-						<div class="p-3 border bg-light">
-							<InformationForm :info="studentInfo" @onChange="onChange"/>
-						</div>
-					</div>
-					<div class="col-6">
-						<div class="p-3 border bg-light h-100"><RequestForm @onRequest="onRequest"/></div>
-					</div>
-					<div class="col">
-						<div class="p-3 border bg-light"><CommentForm /></div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+  <div>
+    <NavStudent />
+    <div class="p-1 active-cont" style="text-align: center">
+      <div class="mx-4">
+        <div class="my-4">
+          <h4>REGISTRAR DIVISION, MAE FAH LUANG UNIVERSITY</h4>
+          <h4>Request Form for Re-entering Student</h4>
+        </div>
+        <!-- เดี๋ยวมาเพิ่ม -->
+        <div>
+          <p>Progress flow</p>
+        </div>
+        <!-- เดี๋ยวมาเพิ่ม -->
+        <div class="row gy-3 mx-5">
+          <div class="col-6">
+            <div class="p-3 border bg-light">
+              <InformationForm :info="studentInfo" @onChange="onChange" />
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="p-3 border bg-light h-100">
+              <RequestForm @onRequest="onRequest" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import NavStudent from "./navStudent.vue";
 import InformationForm from "./form/information.vue";
 import RequestForm from "./form/request.vue";
-import CommentForm from "./form/comment.vue";
-import axios from 'axios';
+
+import axios from "axios";
 export default {
-	data(){
-		return {
-			studentInfo:'',
-			stphone:'',
-			stRequest:'',
-			create_semester:'',
-			create_academic_year:''
-		}
-	},
-	methods:{
-		getstudentInfo(){
-			const path = 'http://127.0.0.1:5000/?id=6131305010';
-			axios.get(path)
-				.then((res)=>{
-					console.log(res.data)
-					this.studentInfo = res.data;
-				})
-				.catch((error)=>{
-					console.log(error)
-				})
-		},
-		sendformInfo(){
-			const path = 'http://127.0.0.1:5000/send209';
-			const formid ={formId:Date.now()}
-			const {student_id,student_advisor_id,student_school,student_name} = this.studentInfo
-			const phone = this.stphone
-			const create_semester = this.create_semester
-			const create_academic_year = this.create_academic_year
-			const senddata = Object.assign({},this.stRequest,formid,{phone,student_id,student_advisor_id,student_school,student_name,create_semester,create_academic_year})
-			this.$confirm("Are you sure?").then(() => {axios.post(path,senddata)
-				.then((res)=>{
-					console.log(res.data)
-					this.$alert("the request had sent")
-				})
-				.catch((error)=>{
-					console.log(error)
-				})
-});
-		},
-		onChange(value){
-			this.stphone = value.phone
-			this.create_semester = value.create_sem
-			this.create_academic_year = value.create_academic_year
-		},
-		onRequest(value){
-			this.stRequest = value
-			console.log(this.stRequest)
-			this.sendformInfo()
-		}
-	},
-	created(){
-		this.getstudentInfo();
-	},
-	components: {
-		NavStudent,
-		InformationForm,
-		RequestForm,
-		CommentForm,
-	},
+  data() {
+    return {
+      studentInfo: "",
+      stphone: "",
+      stRequest: "",
+      create_semester: "",
+      create_academic_year: "",
+    };
+  },
+  methods: {
+    getstudentInfo() {
+      const path = "http://127.0.0.1:5000/?id=6131305010";
+      axios
+        .get(path)
+        .then((res) => {
+          console.log(res.data);
+          this.studentInfo = res.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    sendformInfo() {
+      const path = "http://127.0.0.1:5000/send209";
+      const formid = { formId: Date.now() };
+      const {
+        student_id,
+        student_advisor_id,
+        student_school,
+        student_name,
+      } = this.studentInfo;
+      const phone = this.stphone;
+      const create_semester = this.create_semester;
+      const create_academic_year = this.create_academic_year;
+      const senddata = Object.assign({}, this.stRequest, formid, {
+        phone,
+        student_id,
+        student_advisor_id,
+        student_school,
+        student_name,
+        create_semester,
+        create_academic_year,
+      });
+      this.$confirm("Are you sure?").then(() => {
+        axios
+          .post(path, senddata)
+          .then((res) => {
+            console.log(res.data);
+            this.$alert("the request had sent");
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      });
+    },
+    onChange(value) {
+      this.stphone = value.phone;
+      this.create_semester = value.create_sem;
+      this.create_academic_year = value.create_academic_year;
+    },
+    onRequest(value) {
+      this.stRequest = value;
+      console.log(this.stRequest);
+      this.sendformInfo();
+    },
+  },
+  created() {
+    this.getstudentInfo();
+  },
+  components: {
+    NavStudent,
+    InformationForm,
+    RequestForm,
+  },
 };
 </script>
 
 <style scoped>
 .active-cont {
-	margin-left: 180px;
+  margin-left: 180px;
 }
 </style>
