a3423ee2dda29100df68c8cd3ee943df31bf381e
marnlikeagod016 <marnnew1999@gmail.com>
2021-05-01 12:15:03 +0700

    add config file

---
+++ src/components/config.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <div class="border">
+      <div class="row p-3 align-items-center">
+        <div class="col">
+          <input
+            type="date"
+            v-model="a"
+            class="form-control form-control-edit"
+          />
+        </div>
+        <div class="col">
+          <input
+            type="date"
+            v-model="b"
+            class="form-control form-control-edit"
+          />
+        </div>
+        <div class="col">
+          <input
+            type="date"
+            v-model="c"
+            class="form-control form-control-edit"
+          />
+        </div>
+        <div class="col">
+          <input
+            type="date"
+            v-model="d"
+            class="form-control form-control-edit"
+          />
+        </div>
+      </div>
+      <div class="d-md-flex justify-content-md-end m-3 mt-0">
+        <button type="button" class="btn btn-success">Submit</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
