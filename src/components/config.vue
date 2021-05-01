c0352f8e4ff86308f4984682bd8c54a1d5d09701
THIRAWIT JIRARUNGROJ <6131305010@lamduan.mfu.ac.th>
2021-05-01 15:46:14 +0700

    complete config

--- src/components/config.vue
+++ src/components/config.vue
@@ -5,41 +5,67 @@
         <div class="col">
           <input
             type="date"
-            v-model="a"
+            v-model="startfirst"
             class="form-control form-control-edit"
           />
         </div>
         <div class="col">
           <input
             type="date"
-            v-model="b"
+            v-model="startsecond"
             class="form-control form-control-edit"
           />
         </div>
         <div class="col">
           <input
             type="date"
-            v-model="c"
+            v-model="endfirst"
             class="form-control form-control-edit"
           />
         </div>
         <div class="col">
           <input
             type="date"
-            v-model="d"
+            v-model="endsecond"
             class="form-control form-control-edit"
           />
         </div>
       </div>
       <div class="d-md-flex justify-content-md-end m-3 mt-0">
-        <button type="button" class="btn btn-success">Submit</button>
+        <button type="button" class="btn btn-success" v-on:click="onSubmit">Submit</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+import pathapi from "../pathapi.js";
+export default {
+  data(){
+    return {
+      startfirst:'',
+      startsecond:'',
+      endfirst:'',
+      endsecond:''
+    }
+  },
+  methods:{
+    onSubmit(){
+      const path = pathapi + "/setDate"
+      const senddata = {startfirst:this.startfirst,startsecond:this.startsecond,endfirst:this.endfirst,endsecond:this.endsecond}
+      axios
+          .post(path, senddata)
+          .then((res) => {
+            console.log(res.data);
+            this.$alert("the request had sent");
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+    }
+  }
+};
 </script>
 
 <style>
